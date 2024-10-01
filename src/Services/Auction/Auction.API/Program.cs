c75ca1a30bc77830161d89781391e7e94b3429f3
SirZebo <2957869g@student.gla.ac.uk>
2024-10-01 20:04:52 +0800

    Feats: DI Mediator, Carter and Marten Feats: Added ValidationBehavior Pipeline

--- src/Services/Auction/Auction.API/Program.cs
+++ src/Services/Auction/Auction.API/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to the container. - builder.AddServices()
+builder.Services.AddCarter();
+// Automatically does DI of our handlers through assembly reflection
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline automatically instead of DI in every time a new endpoint is created
+app.MapCarter();
 
 app.Run();
--- src/Services/Bid/Bid.API/Program.cs
+++ src/Services/Bid/Bid.API/Program.cs
@@ -1,9 +1,23 @@
+using BuildingBlocks.Behaviors;
+using Carter;
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
+// Add Services to the container. - builder.AddServices()
+builder.Services.AddCarter();
+// Automatically does DI of our handlers through assembly reflection
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
+// Configure the HTTP request pipeline automatically instead of DI in every time a new endpoint is created
+app.MapCarter();
 
 app.Run();
