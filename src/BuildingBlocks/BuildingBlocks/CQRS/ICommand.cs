9de19ca355069661d8d7e45e9ac7fdd3bab64547
SirZebo <2957869g@student.gla.ac.uk>
2024-10-01 18:47:59 +0800

    Feats: Add BuildingBlocks for CQRS pattern

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS;
+
+// Return a bool for ICommand with no response
+public interface ICommand : ICommand<Unit>
+{
+
+}
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
+
