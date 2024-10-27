f8fea0552b2075e333e71f8d739fffa266548595
SirZebo <sirzebowurks@gmail.com>
2024-10-27 19:49:05 +0800

    Feats: Added BidEndedIntegrationEvent

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/BidEndedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+public record BidEndedEvent : IntegrationEvent
+{
+    public Guid BidId { get; set; }
+    public Guid AuctionId { get; set; }
+    public Guid CustomerId { get; set; }
+    public decimal Price { get; set; }
+}
--- src/Services/Bidding/Bidding.Application/Bids/EventHandlers/Integration/AuctionEndedEventHandler.cs
+++ src/Services/Bidding/Bidding.Application/Bids/EventHandlers/Integration/AuctionEndedEventHandler.cs
@@ -4,13 +4,36 @@ using MassTransit;
 
 namespace Bidding.Application.Bids.EventHandlers.Integration;
 public class AuctionEndedEventHandler
-    (
+    (IPublishEndpoint publisher,
+    IApplicationDbContext dbContext,
     ILogger<AuctionEndedEventHandler> logger)
     : IConsumer<AuctionEndedEvent>
 {
     public async Task Consume(ConsumeContext<AuctionEndedEvent> context)
     {
         logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
-        await Task.CompletedTask;
+
+        var auctionId = AuctionId.Of(context.Message.AuctionId);
+        var bid = await dbContext.Bids.Where(b => b.AuctionId == auctionId).FirstOrDefaultAsync();
+
+        if (bid is null || bid.CustomerId.Value == Guid.Empty)
+        {
+            return; // No one bidded
+        }
+
+        var bidEndedIntegrationEvent = MapToBidEndedIntegrationEvent(bid);
+
+        await publisher.Publish(bidEndedIntegrationEvent);
+    }
+
+    private object MapToBidEndedIntegrationEvent(Bid bid)
+    {
+        return new BidEndedEvent
+        {
+            BidId = bid.Id.Value,
+            AuctionId = bid.AuctionId.Value,
+            CustomerId = bid.CustomerId.Value,
+            Price = bid.Price,
+        };
     }
 }
