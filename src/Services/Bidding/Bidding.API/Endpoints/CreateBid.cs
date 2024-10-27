71472a56c5b208d89828d3d10f31426e2a843a63
SirZebo <sirzebowurks@gmail.com>
2024-10-27 18:53:30 +0800

    Fix: Removed CreateBid Endpoint - This endpoint is only for development

--- src/Services/Bidding/Bidding.API/Endpoints/CreateBid.cs
+++ src/Services/Bidding/Bidding.API/Endpoints/CreateBid.cs
@@ -5,24 +5,24 @@ namespace Bidding.API.Endpoints;
 public record CreateBidRequest(BidDto Bid);
 public record CreateBidResponse(Guid Id);
 
-public class CreateBid : ICarterModule
-{
-    public void AddRoutes(IEndpointRouteBuilder app)
-    {
-        app.MapPost("/bids", async (CreateBidRequest request, ISender sender) =>
-        {
-            var command = request.Adapt<CreateBidCommand>();
+//public class CreateBid : ICarterModule
+//{
+//    public void AddRoutes(IEndpointRouteBuilder app)
+//    {
+//        app.MapPost("/bids", async (CreateBidRequest request, ISender sender) =>
+//        {
+//            var command = request.Adapt<CreateBidCommand>();
 
-            var result = await sender.Send(command);
+//            var result = await sender.Send(command);
 
-            var response = result.Adapt<CreateBidResponse>();
+//            var response = result.Adapt<CreateBidResponse>();
 
-            return Results.Created($"/bids/{response.Id}", response);
-        })
-        .WithName("CreateBid")
-        .Produces<CreateBidResponse>(StatusCodes.Status201Created)
-        .ProducesProblem(StatusCodes.Status400BadRequest)
-        .WithSummary("Create Bid")
-        .WithDescription("Create Bid");
-    }
-}
+//            return Results.Created($"/bids/{response.Id}", response);
+//        })
+//        .WithName("CreateBid")
+//        .Produces<CreateBidResponse>(StatusCodes.Status201Created)
+//        .ProducesProblem(StatusCodes.Status400BadRequest)
+//        .WithSummary("Create Bid")
+//        .WithDescription("Create Bid");
+//    }
+//}
