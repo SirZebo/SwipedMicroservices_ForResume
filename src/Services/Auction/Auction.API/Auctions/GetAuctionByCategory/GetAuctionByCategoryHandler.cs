a44c29c8cad0face730b1c2191b8d8a289ed7837
SirZebo <sirzebowurks@gmail.com>
2024-10-27 15:02:32 +0800

    Feats: Added GetAuctionByCategory Features

---
+++ src/Services/Auction/Auction.API/Auctions/GetAuctionByCategory/GetAuctionByCategoryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Auction.API.Auctions.GetAuctionByCategory;
+
+public record GetAuctionByCategoryQuery(string Category) : IQuery<GetAuctionByCategoryResult>;
+
+public record GetAuctionByCategoryResult(IEnumerable<Models.Auction> Auctions);
+
+public class GetAuctionByCategoryQueryHandler
+    (IDocumentSession session)
+    : IQueryHandler<GetAuctionByCategoryQuery, GetAuctionByCategoryResult>
+{
+    public async Task<GetAuctionByCategoryResult> Handle(GetAuctionByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        var auctions = await session.Query<Models.Auction>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync();
+
+        return new GetAuctionByCategoryResult(auctions);
+    }
+}
