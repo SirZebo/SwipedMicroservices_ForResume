ce2385ba1a1456e3ef7eece0b1bac772baa0150d
SirZebo <2300462@sit.singaporetech.edu.sg>
2024-10-29 17:22:21 +0800

    Feats: Added Distributed Caching in Auction Service

---
+++ src/Services/Auction/Auction.API/Data/IAuctionRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Auction.API.Data;
+
+public interface IAuctionRepository
+{
+    Task<IEnumerable<Models.Auction>> GetAuctions(CancellationToken cancellationToken = default);
+    Task<Models.Auction> GetAuction(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Models.Auction>> GetAuctionByCategory(string category, CancellationToken cancellationToken = default); 
+    Task<Models.Auction> StoreAuction(Models.Auction auction, CancellationToken cancellationToken = default);
+    //Task<bool> DeleteAuction(Guid id, CancellationToken cancellationToken = default);
+}
