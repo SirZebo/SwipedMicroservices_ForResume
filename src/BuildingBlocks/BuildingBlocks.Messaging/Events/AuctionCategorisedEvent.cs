7a445b1ddf01f34c33ecf8efb6f5a613e85d8a56
SirZebo <sirzebowurks@gmail.com>
2024-10-29 17:56:16 +0800

    Feats: Added AuctionCategorisedEvent for ML

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/AuctionCategorisedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingBlocks.Messaging.Events;
+
+public record AuctionCategorisedEvent : IntegrationEvent
+{
+    public Guid AuctionId { get; set; }
+    public required List<string> Category { get; set; }
+}
